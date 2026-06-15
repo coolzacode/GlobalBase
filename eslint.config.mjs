5790793b480124e82b5232be7aa01577e19e1b09
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-06-15 12:44:58 -0500

    fix: eslint document error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default defineConfig([
   js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs'],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: globals.browser,
     },
